928d476e93a3f1facaf97aa676f12d6387759bcf
Bhargav3905 <prajapatibhargavk@gmail.com>
2026-07-03 11:37:47 +0530

    refactor: migrate email service to Brevo API

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,34 +1,9 @@
 import { Router } from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { getOrders } from "../controllers/orderController.js";
-import sendEmail from "../utils/sendEmail.js";
 
 const router = Router();
 
 router.get("/orders", protect, getOrders);
-
-router.get("/test-mail", async (req, res) => {
-    try {
-        console.log("TEST MAIL START");
-
-        await sendEmail(
-            process.env.ADMIN_EMAIL_ID,
-            "Render SMTP Test",
-            "Hello from Render"
-        );
-
-        console.log("MAIL SENT");
-
-        res.json({
-            success: true
-        });
-
-    } catch (err) {
-        console.error("TEST ROUTE ERROR");
-        console.error(err);
-
-        res.status(500).json(err);
-    }
-});
 
 export default router;
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,46 +1,31 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from "nodemailer";
-
-console.log("SMTP_HOST:", process.env.SMTP_HOST);
-console.log("SMTP_PORT:", process.env.SMTP_PORT);
-console.log("SMTP_USER:", process.env.SMTP_USER);
-console.log("ADMIN_EMAIL_ID:", process.env.ADMIN_EMAIL_ID);
-
-const transporter = nodemailer.createTransport({
-    host: "smtp-relay.brevo.com",
-    port: 587,
-    secure: false,
-    family: 4,
-    auth: {
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
-    },
-});
-
-transporter.verify((error, success) => {
-    console.log("===== SMTP VERIFY =====");
-
-    if (error) {
-        console.error(error);
-    } else {
-        console.log("SMTP READY");
-        console.log(success);
-    }
-
-    console.log("=======================");
-});
+import axios from "axios";
 
 const sendEmail = async (to, subject, text) => {
-    await transporter.sendMail({
-        from: `"ShopVerse" <${process.env.ADMIN_EMAIL_ID}>`,
-        to,
-        subject,
-        text,
-    });
-
-    console.log("Email sent");
+    await axios.post(
+        "https://api.brevo.com/v3/smtp/email",
+        {
+            sender: {
+                name: "ShopVerse",
+                email: process.env.ADMIN_EMAIL_ID,
+            },
+            to: [
+                {
+                    email: to,
+                },
+            ],
+            subject,
+            textContent: text,
+        },
+        {
+            headers: {
+                "api-key": process.env.BREVO_API_KEY,
+                "Content-Type": "application/json",
+            },
+        }
+    );
 };
 
 export default sendEmail;
